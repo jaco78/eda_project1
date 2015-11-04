15647450e13f68260982c17e7cc8e420627f772a
jaco78 <jancorluy@hotmail.com>
2015-11-04 18:45:02 +0100

    Change plot2, 3 & 4 to comment out repeated actions from plot1.R

--- plot4.R
+++ plot4.R
@@ -1,17 +1,19 @@
-#download and unzip data
-if(!file.exists("./data")){dir.create("./data")}
-fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(fileUrl, "./data/power_consumption.zip", mode = "wb")
-unzip("./data/power_consumption.zip", exdir = "./data")
+#downloading and reading in dataset commented out - uncomment if necessary (selecting lines 3-16 and using CTRL + SHIFT + C)
 
-#read in dataset and subset for 1/2/2007 and 2/2/2007
-dataset <- read.table("./data/household_power_consumption.txt", header = T, sep = ";", dec = ".", 
-                      colClasses = c(rep("character", 2), rep("numeric", 7)), na.strings = "?")
-dataset <- subset(dataset, Date %in% c("1/2/2007", "2/2/2007"))
-
-#convert date and time colum to POSIXlt column 'date_time'
-dataset$date_time <- with(dataset, paste(Date, Time))
-dataset$date_time <- strptime(dataset$date_time, format = "%d/%m/%Y %H:%M:%S")
+# #download and unzip data
+# if(!file.exists("./data")){dir.create("./data")}
+# fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+# download.file(fileUrl, "./data/power_consumption.zip", mode = "wb")
+# unzip("./data/power_consumption.zip", exdir = "./data")
+# 
+# #read in dataset and subset for 1/2/2007 and 2/2/2007
+# dataset <- read.table("./data/household_power_consumption.txt", header = T, sep = ";", dec = ".", 
+#                       colClasses = c(rep("character", 2), rep("numeric", 7)), na.strings = "?")
+# dataset <- subset(dataset, Date %in% c("1/2/2007", "2/2/2007"))
+# 
+# #convert date and time colum to POSIXlt column 'date_time'
+# dataset$date_time <- with(dataset, paste(Date, Time))
+# dataset$date_time <- strptime(dataset$date_time, format = "%d/%m/%Y %H:%M:%S")
 
 #Create plot4.png
 png("plot4.png", width = 480, height = 480, bg = "transparent")
